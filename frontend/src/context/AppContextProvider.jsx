02dfac83da44cdc0de49779c297cb9ec6e24fdbf
prospermbuma <prospermbuma@gmail.com>
2025-03-17 01:41:07 +0300

    Updates on appointment

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -3,8 +3,11 @@ import { doctors } from '../assets/assets'
 
 const AppContextProvider = ({ children }) => {
 
+    const currencySymbol = '$';
+
     const value = {
-        doctors
+        doctors,
+        currencySymbol
     }
 
     return (
