d9f6b1f5a5ea61587b664f5df659fe8d2bfc36e9
prospermbuma <prospermbuma@gmail.com>
2025-03-16 04:31:05 +0300

    Updated the Not-Found component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Footer = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div className='md:mx-10'>
             <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
@@ -17,10 +21,10 @@ const Footer = () => {
                 <div>
                     <p className='text-xl font-medium mb-5'>COMPANY</p>
                     <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
-                        <li>Privacy Policy</li>
+                        <li onClick={() => {navigate('/'); scrollTo(0,0);}} className='cursor-pointer'>Home</li>
+                        <li onClick={() => {navigate('/about'); scrollTo(0,0);}} className='cursor-pointer'>About Us</li>
+                        <li onClick={() => {navigate('/contact'); scrollTo(0,0);}} className='cursor-pointer'>Contact Us</li>
+                        <li onClick={() => {navigate('/privacy-policy'); scrollTo(0,0);}} className='cursor-pointer'>Privacy Policy</li>
                     </ul>
                 </div>
 
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='mt-10 sm:mt-20'>
+      <div className='w-full h-auto rounded-2xl py-10 sm:py-10 text-center border border-blue-100 text-primary bg-blue-50'>
+        <p className='text-xl sm:text-3xl'>
+          <span>404</span>
+          <br />
+          <b className='text-3xl md:text-5xl'>NOT FOUND</b>
+          <br />
+          <small>Page does not exist.</small>
+        </p>
+      </div>
+    </div>
   )
 }
 
