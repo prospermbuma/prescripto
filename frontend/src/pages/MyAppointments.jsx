01c7a38deb0225bd7be758fefc63e0cc81d1c695
prospermbuma <prospermbuma@gmail.com>
2025-03-19 00:33:58 +0300

    More updates on My-Appointments

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -7,7 +7,7 @@ const MyAppointments = () => {
 
   return (
     <div>
-      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b border-gray-300'>My appointment</p>
+      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b border-gray-300'>My appointments</p>
       <div>
         {doctors.slice(0, 3).map((item) => (
           <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b border-gray-300' key={item._id}>
