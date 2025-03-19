21849fe413d555252673ed59e3a841163ca15fba
prospermbuma <prospermbuma@gmail.com>
2025-03-19 11:27:05 +0300

    More

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
 
 // App config
 const app = express();
 const PORT = process.env.PORT || 4000;
+connectDB();
 
 // Middleware
 app.use(express.json());
