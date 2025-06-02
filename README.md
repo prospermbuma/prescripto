22cb66a7c08383622b30584b44e488f7dda70150
prospermbuma <prospermbuma@gmail.com>
2025-06-02 09:27:52 +0300

    UPDATES

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Prescripto
 Is the doctor appointment online platform that allows patients to book appointment fot health consultancy.
+
+
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 // App config
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
+connectCloudinary();
 
 // Middleware
 app.use(express.json());
